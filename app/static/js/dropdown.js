7dd7c5938b34ffd5cd43f31b8a547044b452451e
JmThrCreator <jmthrcreator@gmail.com>
2022-11-23 17:55:00 +0000

    fixed errors

--- app/static/js/dropdown.js
+++ app/static/js/dropdown.js
@@ -2,13 +2,21 @@
 
 function dropdown() {
     var dropdown = document.getElementById("dropdown");
+    var buttons = document.getElementsByClassName("sort_button");
+    console.log(buttons)
     if (dropdown.style.opacity == "100") {
         dropdown.style.opacity = "0";
         dropdown.style.scale = "95%";
+        for (var i = 0; i < buttons.length; i++) {
+            buttons[i].style.pointerEvents = "none";
+        }
     }
     else {
         dropdown.style.opacity = "100";
         dropdown.style.scale = "100%";
+        for (var i = 0; i < buttons.length; i++) {
+            buttons[i].style.pointerEvents = "auto";
+        }
     }
 }
 
@@ -16,9 +24,14 @@ window.onclick = function(event) {
     var targetId = event.target.id
     if (targetId !== "dropdown_button" && targetId !== "dropdown") {
         var dropdown = document.getElementById("dropdown");
+        var buttons = document.getElementsByClassName("sort_button");
         if (dropdown.style.opacity == "0") {
             dropdown.style.opacity = "0";
             dropdown.style.scale = "95%";
+            dropdown.style.pointerEvents = "none";
+            for (var i = 0; i < buttons.length; i++) {
+                buttons[i].style.pointerEvents = "none";
+            }
         }
     }
 }
